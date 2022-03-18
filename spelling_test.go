134f3c2bbf828eecb11693bfcbe8c75b709e2e05
Jorge Osorio <jorelosorio@gmail.com>
2022-03-18 18:54:26 +0100

    Update spelling_test.go

--- spelling_test.go
+++ spelling_test.go
@@ -6,8 +6,20 @@ import (
 
 var spelling = NewSpelling("./dictionaries/es.dic")
 
-func TestCorrection(t *testing.T) {
+func TestCorrection_FoundCorrection(t *testing.T) {
 	if spelling.Correction("espanol") != "español" {
 		t.Error("Expected returned string to be 'español'")
+	}
+}
+
+func TestCorrection_CorrectWord(t *testing.T) {
+	if spelling.Correction("español") != "español" {
+		t.Error("Expected returned string to be 'español'")
+	}
+}
+
+func TestCorrection_NotFound(t *testing.T) {
+	if spelling.Correction("jorge") != "jorge" {
+		t.Error("Expected returned string to be 'jorge'")
 	}
 }
