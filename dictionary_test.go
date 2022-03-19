5abdc9c61d22530706cc3b05ade354212358da84
Jorge Osorio <jorelosorio@gmail.com>
2022-03-19 15:00:53 +0100

    Update dictionary_test.go

--- dictionary_test.go
+++ dictionary_test.go
@@ -21,11 +21,48 @@ func newDictionary(t *testing.T) *Dictionary {
 	return dic
 }
 
-func TestTrain(t *testing.T) {
+func TestLoadDictionary(t *testing.T) {
+	currentPath, err := os.Getwd()
+	if err != nil {
+		t.Error(err)
+	}
+
+	dictionaryFilePath := path.Join(currentPath, "dictionaries", "wrong.dic")
+	_, err = LoadDictionary(dictionaryFilePath)
+
+	if err == nil {
+		t.Error("If the dictionary file does not exist, then it should fail")
+	}
+}
+
+func TestTrainDictionary(t *testing.T) {
+	currentPath, err := os.Getwd()
+	if err != nil {
+		t.Error(err)
+	}
+
+	textFilePath := path.Join(currentPath, "examples", "test_sample1.txt")
+
 	dic := newDictionary(t)
-	dic.TrainFromTextFile("./examples/test_sample1.txt")
+	dic.TrainFromTextFile(textFilePath)
 
 	if len(dic.Words) != 2 {
 		t.Error("Expected returned length should be 2")
+	}
+}
+
+func TestTrainDictionaryWithWrongText(t *testing.T) {
+	currentPath, err := os.Getwd()
+	if err != nil {
+		t.Error(err)
+	}
+
+	textFilePath := path.Join(currentPath, "examples", "wrong.txt")
+
+	dic := newDictionary(t)
+	err = dic.TrainFromTextFile(textFilePath)
+
+	if err == nil {
+		t.Error("If the text file does not exist, then it should fail")
 	}
 }
