a17c9a8a28f1211f8f282c21245cce6d139c9175
Jorge Osorio <jorelosorio@gmail.com>
2022-03-19 10:59:03 +0100

    Update spelling_test.go

--- spelling_test.go
+++ spelling_test.go
@@ -4,9 +4,22 @@ import (
 	"testing"
 )
 
-var spelling, _ = NewSpelling("./dictionaries/es.dic")
+const (
+	dictionaryFilePath = "./dictionaries/es.dic"
+)
+
+func newSpelling(t *testing.T) *Spelling {
+	spelling, err := NewSpelling(dictionaryFilePath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	return spelling
+}
 
 func TestCorrectedWord(t *testing.T) {
+	spelling := newSpelling(t)
+
 	if spelling.Correction("espanol") != "español" {
 		t.Error("Expected returned string to be 'español'")
 	}
@@ -17,18 +30,24 @@ func TestCorrectedWord(t *testing.T) {
 }
 
 func TestExistingWord(t *testing.T) {
+	spelling := newSpelling(t)
+
 	if spelling.Correction("español") != "español" {
 		t.Error("Expected returned string to be 'español'")
 	}
 }
 
 func TestWordNotFound(t *testing.T) {
+	spelling := newSpelling(t)
+
 	if spelling.Correction("jorge") != "jorge" {
 		t.Error("Expected returned string to be 'jorge'")
 	}
 }
 
 func TestLongWordNotFound(t *testing.T) {
+	spelling := newSpelling(t)
+
 	if spelling.Correction("calculadorase") != "calculadorase" {
 		t.Error("Expected returned string to be 'calculadorase'")
 	}
