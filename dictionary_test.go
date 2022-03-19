be7493eb738f3c91c05166fcbb44db9a843d1a41
Jorge Osorio <jorelosorio@gmail.com>
2022-03-19 11:01:57 +0100

    Update dictionary_test.go

--- dictionary_test.go
+++ dictionary_test.go
@@ -5,7 +5,11 @@ import (
 )
 
 func TestTrain(t *testing.T) {
-	dic, _ := NewDictionary("./tmp/es.dic", ESAlphabet)
+	dic, err := NewDictionary("./tmp/es.dic", ESAlphabet)
+	if err != nil {
+		t.Error(err)
+	}
+
 	dic.TrainFromTextFile("./examples/test_sample1.txt")
 
 	if len(dic.Words) != 2 {
