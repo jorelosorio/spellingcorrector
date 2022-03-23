ba73cf6109038c1998e5b54ec715fe30c044ff2d
Jorge Osorio <jorelosorio@gmail.com>
2022-03-23 10:30:15 +0100

    Update spelling.go

--- spelling.go
+++ spelling.go
@@ -26,66 +26,65 @@ func (s *Spelling) Correction(word string) string {
 		return word
 	}
 
-	if correction := s.selectBestFor(word, s.edits1(word)); correction != "" {
-		return correction
+	words := make(chan string)
+	maxFreq := 0
+	correction := ""
+
+	go s.genAlternativesOf(word, words, true)
+
+	for w := range words {
+		if w == "" {
+			break
+		}
+
+		if freq, present := s.dic.Words[w]; present && freq > maxFreq {
+			maxFreq, correction = freq, w
+		}
 	}
 
-	if correction := s.selectBestFor(word, s.edits2(word)); correction != "" {
+	if correction != "" {
 		return correction
 	}
 
 	return word
 }
 
-func (s *Spelling) edits1(word string) []string {
-	var splits [][]string
+func (s *Spelling) genAlternativesOf(word string, words chan string, expand bool) {
+	splits := [][]string{}
 	for i := 0; i < len(word)+1; i++ {
 		splits = append(splits, []string{word[:i], word[i:]})
 	}
 
-	var words []string
-	for _, v := range splits {
-		l, r := v[0], v[1]
+	callGenAltNoExpandWith := func(wordToExpand string) string {
+		if expand {
+			go s.genAlternativesOf(wordToExpand, words, false)
+		}
+
+		return wordToExpand
+	}
+
+	for _, wordPair := range splits {
+		l, r := wordPair[0], wordPair[1]
 		lr := len(r)
+
 		if lr > 0 {
-			// Deletes
-			words = append(words, l+r[1:])
+			words <- callGenAltNoExpandWith(l + r[1:])
 		}
+
 		if lr > 1 {
-			// Transposes
-			words = append(words, l+string(r[1])+string(r[0])+r[2:])
+			words <- callGenAltNoExpandWith(l + string(r[1]) + string(r[0]) + r[2:])
 		}
+
 		for _, c := range s.dic.Alphabet {
 			if lr > 0 {
-				// Replaces
-				words = append(words, l+string(c)+r[1:])
+				words <- callGenAltNoExpandWith(l + string(c) + r[1:])
 			}
 
-			// Inserts
-			words = append(words, l+string(c)+r)
+			words <- callGenAltNoExpandWith(l + string(c) + r)
 		}
 	}
 
-	return words
-}
-
-func (s *Spelling) edits2(word string) []string {
-	var e2 []string
-	for _, e1 := range s.edits1(word) {
-		e2 = append(e2, s.edits1(e1)...)
+	if expand {
+		words <- ""
 	}
-
-	return e2
-}
-
-func (s *Spelling) selectBestFor(word string, words []string) string {
-	maxFreq := 0
-	correction := ""
-	for _, word := range words {
-		if freq, present := s.dic.Words[word]; present && freq > maxFreq {
-			maxFreq, correction = freq, word
-		}
-	}
-
-	return correction
 }
